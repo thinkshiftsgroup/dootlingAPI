5b3d4199276cc7d108c8b96cd5e9667175cb2490
denzubechi <chukwumasamuel371@gmail.com>
2025-11-02 17:55:19 +0100

    fix:user fetch err

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getUserProfileController } from "@controllers/user.controller";
+import {
+  getUserProfileController,
+  getPublicProfileByUsernameController,
+} from "@controllers/user.controller";
 
 import asyncHandler from "@utils/asyncHandler";
 const router = Router();
 
 router.get("/:userId", asyncHandler(getUserProfileController));
-router.get("/username/:username", asyncHandler(getUserProfileController));
+router.get(
+  "/username/:username",
+  asyncHandler(getPublicProfileByUsernameController)
+);
 
 export const userRouter = router;
